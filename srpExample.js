e55d3d8dda22fbc381c31e257ffd728390bfe8ca
DeivydCharles <deicycharlles08@gmail.com>
2025-11-13 09:45:52 -0300

    Adicionando Atividade

---
+++ srpExample.js
@@ -0,0 +1,43 @@
+// --- Exemplo em JavaScript: (S) Single Responsibility Principle ---
+
+class UserViolation {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    saveToDatabase() {
+
+        console.log(`Salvando ${this.name} no banco de dados...`);
+    }
+}
+
+// -----------------------------------------------------------------
+
+// ✅ APLICAÇÃO DO SRP
+
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+}
+
+class UserRepository {
+    save(user) {
+
+        console.log(`Salvando ${user.getName()} no banco de dados...`);
+    }
+}
+
+const user = new User("Alice", "alice@example.com");
+const repository = new UserRepository();
+repository.save(user);
